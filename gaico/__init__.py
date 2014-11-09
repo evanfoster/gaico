e0b0f69416ae6be3151043217d74bb71205f9815
Laurent Meunier <laurent@deltalima.net>
2014-11-09 13:08:11 +0100

    Add docstring for module gaico

--- gaico/__init__.py
+++ gaico/__init__.py
@@ -1,3 +1,14 @@
 # -*- coding: utf-8 -*-
 
+"""
+    Gaico
+    ~~~~~
+
+    Gaico is a collection of small useful functions. Nothing fancy or
+    revolutionary, just little things I use in several projects. The primary
+    goal of this package is to provide pure python implementations of network
+    tools (like ping).
+
+"""
+
 __version__ = '0.1.0'
